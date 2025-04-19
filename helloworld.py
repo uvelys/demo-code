39874ca079a1fdb02deebb5f40b0114dc542c20d
uvelys <uvelys@test.dom>
2025-04-20 02:10:30 +0900

    init

---
+++ helloworld.py
@@ -0,0 +1,12 @@
+class Message:
+    def printMessage(self):
+        """
+        A simple function that prints a message
+        """
+        print("Hello World!")
+
+
+if __name__ == "__main__":
+    msg = Message()
+    msg.printMessage()
+
